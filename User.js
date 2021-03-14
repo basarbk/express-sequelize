17b18daafe2652abaf0444d819f06c99bc3acbb3
Basar Buyukkahraman <basarbk@yahoo.com>
2021-03-14 20:59:48 +0300

    Add User model

---
+++ User.js
@@ -0,0 +1,21 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('./database');
+
+class User extends Model {};
+
+User.init({
+  username: {
+    type: DataTypes.STRING
+  },
+  email: {
+    type: DataTypes.STRING
+  },
+  password: {
+    type: DataTypes.STRING
+  }
+}, {
+  sequelize,
+  modelName: 'user'  
+})
+
+module.exports = User;
--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const sequelize = require('./database');
+const User = require('./User');
 
 sequelize.sync().then(() => console.log('db is ready'));
 
 const app = express();
+
+app.use(express.json());
+
+app.post('/users', (req, res) => {
+  User.create(req.body).then(() => {
+    res.send("success")
+  })
+})
 
 app.listen(3000, () => {
   console.log("app is running");
