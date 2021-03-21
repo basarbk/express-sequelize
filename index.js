29f3eedb3c6ac89ad187dccb2e286bafb9b7b954
Basar Buyukkahraman <basarbk@yahoo.com>
2021-03-21 16:56:05 +0300

    Use offset

--- index.js
+++ index.js
@@ -24,7 +24,8 @@ app.post('/users', async (req, res) => {
 
 app.get('/users', async (req, res) => {
   const usersWithCount = await User.findAndCountAll({
-    limit: 5
+    limit: 5,
+    offset: 10
   });
   res.send(usersWithCount);
 })
