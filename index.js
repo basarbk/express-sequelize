0617464aff873b2a7bf68f5846881e0f98a14af4
Basar Buyukkahraman <basarbk@yahoo.com>
2021-04-18 13:51:18 +0300

    Install dependency and cleanup

--- index.js
+++ index.js
@@ -1,25 +1,7 @@
 const sequelize = require('./src/config/database');
-const User = require('./src/user/User');
-const Article = require('./src/article/Article');
 const app = require('./src/app');
 
-
-sequelize.sync({ force: true }).then(async () => {
-  for(let i = 1; i <= 15; i++){
-    const user = {
-      username: `user${i}`,
-      email: `user${i}@mail.com`,
-      password: 'P4ssword'
-    }
-    await User.create(user);
-    const article = {
-      content: `article content ${i}`
-    }
-    await Article.create(article);
-  }
-});
-
-
+sequelize.sync({ force: true });
 
 app.listen(3000, () => {
   console.log("app is running");
