4020b2e9695f4356e56968e315dd515e7e874536
Basar Buyukkahraman <basarbk@yahoo.com>
2021-04-25 14:20:07 +0300

    Encrypt password with bcrypt

--- src/user/UserService.js
+++ src/user/UserService.js
@@ -1,8 +1,11 @@
 const User = require('./User');
 const UserNotFoundException = require('./UserNotFoundException');
+const bcrypt = require('bcrypt');
 
 const create = async (body) => {
-  await User.create(body);
+  const { username, email, password } = body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  await User.create({ username, email, password: hashedPassword});
 }
 
 const getUsers = async (pagination) => {
