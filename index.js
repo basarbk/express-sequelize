6eeadc29f243bf410ed9f6f0b0296f7333beb6ce
Basar Buyukkahraman <basarbk@yahoo.com>
2021-05-02 12:21:21 +0300

    Create users

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const sequelize = require('./src/config/database');
 const app = require('./src/app');
+const UserService = require('./src/user/UserService');
 
-sequelize.sync({ force: true });
+sequelize.sync({ force: true }).then(async () => {
+  for(let i = 1; i <= 5; i++) {
+    const user = {
+      username: `user${i}`,
+      email: `user${i}@mail.com`,
+      password: 'P4ssword'
+    }
+    await UserService.create(user);
+  }
+});
 
 app.listen(3000, () => {
   console.log("app is running");
