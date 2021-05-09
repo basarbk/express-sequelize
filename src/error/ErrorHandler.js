9363b957a1e6406a00aa680f1e4451700637ecca
Basar Buyukkahraman <basarbk@yahoo.com>
2021-05-09 14:43:27 +0300

    Translation for UserNotFoundException

--- src/error/ErrorHandler.js
+++ src/error/ErrorHandler.js
@@ -9,7 +9,7 @@ module.exports = (err, req, res, next) => {
     .status(status)
     .send(
       {
-        message: message,
+        message: req.t(message),
         timestamp: Date.now(),
         path: req.originalUrl,
         validationErrors
