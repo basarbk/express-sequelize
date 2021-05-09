749725aa2464e5d12126d6c5769fb48f89a62a0e
Basar Buyukkahraman <basarbk@yahoo.com>
2021-05-09 14:47:54 +0300

    Translate ValidationException

--- src/shared/ValidationException.js
+++ src/shared/ValidationException.js
@@ -1,5 +1,5 @@
 module.exports = function ValidationException(errors){
   this.status = 400;
-  this.message = 'Invalid Request';
+  this.message = 'validation_exception';
   this.errors = errors;
 }
