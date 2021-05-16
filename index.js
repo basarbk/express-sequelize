fde101439159032588108bed703351ab7e957eca
Basar Buyukkahraman <basarbk@yahoo.com>
2021-05-16 12:34:08 +0300

    Use cross-env

--- index.js
+++ index.js
@@ -2,17 +2,21 @@ const sequelize = require('./src/config/database');
 const app = require('./src/app');
 const UserService = require('./src/user/UserService');
 
-sequelize.sync({ force: true }).then(async () => {
-  for(let i = 1; i <= 5; i++) {
-    const user = {
-      username: `user${i}`,
-      email: `user${i}@mail.com`,
-      password: 'P4ssword'
+if(process.env.NODE_ENV === 'production') {
+  sequelize.sync();
+} else {
+  sequelize.sync({ force: true }).then(async () => {
+    for(let i = 1; i <= 5; i++) {
+      const user = {
+        username: `user${i}`,
+        email: `user${i}@mail.com`,
+        password: 'P4ssword'
+      }
+      await UserService.create(user);
     }
-    await UserService.create(user);
-  }
-});
+  });
+}
 
 app.listen(3000, () => {
-  console.log("app is running");
+  console.log("app is running in mode: ", process.env.NODE_ENV);
 });
--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,30 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('test-db', 'user', 'pass', {
+
+let configuration = {
+  db: 'test-db',
+  username: 'user',
+  password: 'pass',
   dialect: 'sqlite',
-  host: './dev.sqlite'
+  host: './dev.sqlite',
+  logging: true
+}
+
+if(process.env.NODE_ENV === 'production'){
+  configuration = {
+    db: 'production-db',
+    username: 'prod-user',
+    password: 'prod-pass',
+    dialect: 'sqlite',
+    host: './prod.sqlite',
+    logging: false
+  }
+}
+
+const sequelize = new Sequelize(configuration.db, configuration.username, configuration.password, {
+  dialect: configuration.dialect,
+  host: configuration.host,
+  logging: configuration.logging
 })
 
 module.exports = sequelize;
