bb5e63f384b696f13fd0abac1c6aeea80ee5d168
Basar Buyukkahraman <basarbk@yahoo.com>
2021-05-16 12:43:16 +0300

    Use config

--- src/config/database.js
+++ src/config/database.js
@@ -1,25 +1,7 @@
 const { Sequelize } = require('sequelize');
+const config = require('config');
 
-
-let configuration = {
-  db: 'test-db',
-  username: 'user',
-  password: 'pass',
-  dialect: 'sqlite',
-  host: './dev.sqlite',
-  logging: true
-}
-
-if(process.env.NODE_ENV === 'production'){
-  configuration = {
-    db: 'production-db',
-    username: 'prod-user',
-    password: 'prod-pass',
-    dialect: 'sqlite',
-    host: './prod.sqlite',
-    logging: false
-  }
-}
+const configuration = config.get("database");
 
 const sequelize = new Sequelize(configuration.db, configuration.username, configuration.password, {
   dialect: configuration.dialect,
