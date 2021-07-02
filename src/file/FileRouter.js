d30407a81d5aa10ca3da1150eca61be38ce67048
Basar Buyukkahraman <basarbk@yahoo.com>
2021-07-02 17:19:12 +0300

    Set file size limitation

--- src/file/FileRouter.js
+++ src/file/FileRouter.js
@@ -3,12 +3,18 @@ const router = express.Router();
 const multer = require('multer');
 const fs = require('fs');
 
-const upload = multer();
+const upload = multer({limits: {fileSize: 1024*1024}}).single("file");
 
-router.post('/upload', upload.single("file"), async (req, res) => {
-  const file = req.file;
-  await fs.promises.writeFile('./upload-dir/uploaded-file.png', file.buffer);
-  res.send();
+router.post('/upload', (req, res) => {
+  upload(req, res, async (err) => {
+    if(err){
+      return res.status(400).send({message: "Size too big"})
+    }
+    const file = req.file;
+    console.log(req.file);
+    await fs.promises.writeFile('./upload-dir/uploaded-file.png', file.buffer);
+    res.send({message: "Upload success"});
+  })
 });
 
 module.exports = router;
