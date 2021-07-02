3bb64abd1fbc6b31fac4d6be55450e446db7db9e
Basar Buyukkahraman <basarbk@yahoo.com>
2021-07-02 17:35:15 +0300

    Add transport

--- src/logger.js
+++ src/logger.js
@@ -1,4 +1,8 @@
-const { createLogger } = require('winston');
-const logger = createLogger();
+const { createLogger, transports } = require('winston');
+const logger = createLogger({
+  transports: [
+    new transports.Console()
+  ]
+});
 
 module.exports = logger;
