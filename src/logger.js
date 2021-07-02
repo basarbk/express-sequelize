32a12f0c0009cb62f98290cf67e104e760a6f11b
Basar Buyukkahraman <basarbk@yahoo.com>
2021-07-02 17:48:20 +0300

    Set format

--- src/logger.js
+++ src/logger.js
@@ -1,6 +1,11 @@
-const { createLogger, transports } = require('winston');
+const { createLogger, transports, format } = require('winston');
+
+const customFormat = format.combine(format.timestamp(), format.printf((info) => {
+  return `${info.timestamp} [${info.level.toUpperCase().padEnd(7)}]: ${info.message}`
+}))
+
 const logger = createLogger({
-  level: 'debug',
+  format: customFormat,
   transports: [
     new transports.Console({level: 'silly'}),
     new transports.File({ filename: 'app.log', level: 'info'})
