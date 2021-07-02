5734bf7148a219c14ea07451facab69d743a4591
Basar Buyukkahraman <basarbk@yahoo.com>
2021-07-02 17:09:27 +0300

    Add multer middleware

--- src/file/FileRouter.js
+++ src/file/FileRouter.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
 
-router.post('/upload', (req, res) => {
+const upload = multer();
 
+router.post('/upload', upload.single("file"), (req, res) => {
+  console.log(req.file);
+  res.send();
 });
 
 module.exports = router;
