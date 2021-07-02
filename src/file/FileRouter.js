73e3ff68bbedcc79c17550c991d3e9e40be0ce6d
Basar Buyukkahraman <basarbk@yahoo.com>
2021-07-02 17:18:42 +0300

    Save file to folder

--- src/file/FileRouter.js
+++ src/file/FileRouter.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
+const fs = require('fs');
 
 const upload = multer();
 
-router.post('/upload', upload.single("file"), (req, res) => {
-  console.log(req.file);
+router.post('/upload', upload.single("file"), async (req, res) => {
+  const file = req.file;
+  await fs.promises.writeFile('./upload-dir/uploaded-file.png', file.buffer);
   res.send();
 });
 
